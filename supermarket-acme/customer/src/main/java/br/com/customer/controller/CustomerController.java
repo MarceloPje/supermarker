d300acad299e8b5252a3b2411917e64c9ad62d3a
CarlosBarbosaFilho <cbarbosagomesfilho@gmail.com>
2022-05-07 15:18:05 -0300

    include openapi to customer services

--- supermarket-acme/customer/src/main/java/br/com/customer/controller/CustomerController.java
+++ supermarket-acme/customer/src/main/java/br/com/customer/controller/CustomerController.java
@@ -4,12 +4,18 @@ import br.com.customer.config.ConvertUtils;
 import br.com.customer.controller.request.CustomerRequest;
 import br.com.customer.model.CustomerEntity;
 import br.com.customer.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/customers")
+@Tag(name = "Api to management customers", description = "Create and management customers")
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -19,9 +25,12 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @Operation(summary = "create customer", description = "create customer to fraud system")
+    @ApiResponse(responseCode = "201", description = "Customer success created")
     @PostMapping
-    public void createCustomer(@RequestBody CustomerRequest customerRequest) {
+    public ResponseEntity<?> createCustomer(@RequestBody CustomerRequest customerRequest) {
         log.info("calling controller to create customer {}", customerRequest );
         this.customerService.createCustomer(customerRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
